5a955f289872ee56d3fde057d2dc6d3642945011
vitusbt <vitusbt@gmail.com>
2024-01-03 14:38:47 +0100

    dataset v2

--- mnist-exercise/data/make_dataset.py
+++ mnist-exercise/data/make_dataset.py
@@ -6,30 +6,16 @@ from torch.utils.data import TensorDataset, DataLoader
 if __name__ == '__main__':
     # Get the data and process it
 
-    train_images_0 = torch.load('data/raw/train_images_0.pt')
-    train_images_1 = torch.load('data/raw/train_images_1.pt')
-    train_images_2 = torch.load('data/raw/train_images_2.pt')
-    train_images_3 = torch.load('data/raw/train_images_3.pt')
-    train_images_4 = torch.load('data/raw/train_images_4.pt')
-    train_images_5 = torch.load('data/raw/train_images_5.pt')
-    train_target_0 = torch.load('data/raw/train_target_0.pt')
-    train_target_1 = torch.load('data/raw/train_target_1.pt')
-    train_target_2 = torch.load('data/raw/train_target_2.pt')
-    train_target_3 = torch.load('data/raw/train_target_3.pt')
-    train_target_4 = torch.load('data/raw/train_target_4.pt')
-    train_target_5 = torch.load('data/raw/train_target_5.pt')
+    train_images = []
+    train_target = []
+    for i in range(10):
+        train_images.append(torch.load(f'data/raw/train_images_{i}.pt'))
+        train_target.append(torch.load(f'data/raw/train_target_{i}.pt'))
 
     test_images = torch.load('data/raw/test_images.pt').unsqueeze(1)
     test_target = torch.load('data/raw/test_target.pt')
 
-    train_images = torch.concat([
-        train_images_0,
-        train_images_1,
-        train_images_2,
-        train_images_3,
-        train_images_4,
-        train_images_5
-    ]).unsqueeze(1)
+    train_images = torch.concat(train_images).unsqueeze(1)
 
     #train_images = train_images*2 - 1
     #test_images = test_images*2 - 1
@@ -40,14 +26,7 @@ if __name__ == '__main__':
     train_images = (train_images - train_mean) / train_std
     test_images = (test_images - train_mean) / train_std
 
-    train_target = torch.concat([
-        train_target_0,
-        train_target_1,
-        train_target_2,
-        train_target_3,
-        train_target_4,
-        train_target_5
-    ])
+    train_target = torch.concat(train_target)
 
     torch.save(train_images, "data/processed/train_images.pt")
     torch.save(test_images, "data/processed/test_images.pt")
